0198d153a0b6c224ca1389390580c11541319b68
Mohamed Badr <60277974+Mohamed-A-Badr@users.noreply.github.com>
2022-02-25 00:06:17 +0200

    Create main.cpp

---
+++ Task-1/problem-6/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define fast_ios ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_in freopen("input.txt", "r", stdin);
+#define file_out freopen("output.txt", "w", stdout);
+#define el "\n"
+#define cin(v) for(auto &it : (v)) cin >> it;
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+const ld MOD = 1000000007, E = 2.718281828459045, PI = acos(-1);
+
+void fast(){
+#ifndef ONLINE_JUDGE
+    file_in
+    file_out
+#endif
+    fast_ios
+}
+
+
+void solve(){
+    string s;
+    getline(cin, s);
+    cout << "gzuz" << el;
+}
+
+int main(){
+    fast();
+    //fast_ios
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
